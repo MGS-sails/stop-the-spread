69bda5988df7b2e7ce9eb99c36a4449739227e84
mahfouz shehu <mahfouzshehu@mahfouzs-MacBook-Pro-2.local>
2020-03-30 06:02:26 +0100

    added google auth

--- src/App.vue
+++ src/App.vue
@@ -177,6 +177,7 @@
                             // User successfully signed in.
                             // Return type determines whether we continue the redirect automatically
                             // or whether we leave that to developer to handle.
+                            window.location.reload();
                             return true;
                         },
                         uiShown: function () {
@@ -191,10 +192,10 @@
                     signInOptions: [
                         // Leave the lines as is for the providers you want to offer your users.
                         firebase.auth.EmailAuthProvider.PROVIDER_ID,
-                        firebase.auth.PhoneAuthProvider.PROVIDER_ID,
                         firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-                        firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-                        firebase.auth.TwitterAuthProvider.PROVIDER_ID
+                        // firebase.auth.PhoneAuthProvider.PROVIDER_ID,
+                        // firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+                        // firebase.auth.TwitterAuthProvider.PROVIDER_ID
                     ],
                     // Terms of service url.
                     tosUrl: '<your-tos-url>',
